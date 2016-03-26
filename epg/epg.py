db6e8761e6586b5b8e861758474939d509004b68
Jon Tamir <jtamir@eecs.berkeley.edu>
2016-03-25 18:28:53 -0700

    update to match epg-cpmg

--- epg/epg.py
+++ epg/epg.py
@@ -3,58 +3,107 @@
 # EPG Simulation code, based off of Matlab scripts from Brian Hargreaves <bah@stanford.edu>
 # 2015 Jonathan Tamir <jtamir@eecs.berkeley.edu>
 
+from __future__ import division
 import numpy as np
 from numpy import pi, cos, sin, exp, conj
 from warnings import warn
 
-def epg_rf(FpFmZ, alpha, phi):
+def rf(FpFmZ, alpha, phi):
+    "Same as rf2, but only returns FpFmZ"""
+    return rf2(FpFmZ, alpha, phi)[0]
+
+def rf2(FpFmZ, alpha, phi):
     """ Propagate EPG states through an RF rotation of 
     alpha, with phase phi (both radians).
 
     INPUT:
         FpFmZ = 3xN vector of F+, F- and Z states.
-		  alpha, phi = RF pulse flip angle phase in radians
-
-    OUTPUT:
-        FpFmZ = Updated FpFmZ state.
-
-    SEE ALSO:
-    epg_grad, epg_grelax
-    """
-
-    return epg_rf2(FpFmZ, alpha, phi)[0]
-
-def epg_rf2(FpFmZ, alpha, phi):
-    """ Propagate EPG states through an RF rotation of 
-    alpha, with phase phi (both radians).
-
-    INPUT:
-        FpFmZ = 3xN vector of F+, F- and Z states.
-		  alpha, phi = RF pulse flip angle phase in radians
+        alpha, phi = RF pulse flip angle phase in radians
 
     OUTPUT:
         FpFmZ = Updated FpFmZ state.
         RR = RF rotation matrix (3x3).
 
-    SEE ALSO:
-    epg_grad, epg_grelax
     """
 
-    # -- From Weigel at al, JMR 205(2010)276-285, Eq. 8.
+    # -- From Weigel at al, JMRI 41(2015)266-295, Eq. 15.
     
     if abs(alpha) > 2 * pi:
-        warn('epg_rf: Flip angle should be in radians!')
+        warn('rf: Flip angle should be in radians!')
 
-    RR = np.array([ [ (cos(alpha/2.))**2, exp(2*1j*phi)*(sin(alpha/2.))**2, -1j*exp(1j*phi)*sin(alpha)],
-            [exp(-2*1j*phi)*(sin(alpha/2.))**2, (cos(alpha/2.))**2, 1j*exp(-1j*phi)*sin(alpha)],
-            [-1j/2.*exp(-1j*phi)*sin(alpha), 1j/2.*exp(1j*phi)*sin(alpha), cos(alpha)] ])
+    cosa2 = cos(alpha/2.)**2
+    sina2 = sin(alpha/2.)**2
+    cosa = cos(alpha)
+    sina = sin(alpha)
+
+    ejp = exp(1j * phi)
+    e2jp = exp(2j * phi)
+    emjp = exp(-1j * phi)
+    em2jp = exp(-2j * phi)
+
+    RR = np.array([ [ cosa2, e2jp * sina2, -1j * ejp * sina],
+            [em2jp * sina2, cosa2, 1j * emjp * sina],
+            [-1j/2. * emjp * sina, 1j/2. * ejp * sina, cosa] ])
+
 
     FpFmZ = np.dot(RR , FpFmZ)
 
     return FpFmZ, RR
 
 
-def epg_relax(FpFmZ, T1, T2, T):
+def rf_prime(FpFmZ, alpha, phi):
+    """Same as rf_prime, but only returns FpFmZ"""
+    return rf_prime2(FpFmZ, alpha, phi)[0]
+
+def rf_prime2(FpFmZ, alpha, phi):
+    """ Compute the gradient of the RF rotation operator w.r.t. alpha, where
+    alpha and phase (radians) are the RF rotation and phase. Phase is assumed
+    to be a constant
+
+    INPUT:
+        FpFmZ = 3xN vector of F+, F- and Z states.
+        alpha, phi = RF pulse flip angle and phase in radians
+
+    OUTPUT:
+        FpFmZ = Derivative of FpFmZ state w.r.t. alpha
+        RR = Derivative of RF rotation matrix (3x3) w.r.t. alpha
+
+    """
+
+    if abs(alpha) > 2 * pi:
+        warn('rf2: Flip angle should be in radians!')
+
+
+    cosa2 = cos(alpha/2.)
+    sina2 = sin(alpha/2.)
+    cosa = cos(alpha)
+    sina = sin(alpha)
+
+    ejp = exp(1j * phi)
+    e2jp = exp(2j * phi)
+    emjp = exp(-1j * phi)
+    em2jp = exp(-2j * phi)
+
+
+    RR = np.array([ [-cosa2 * sina2, e2jp * cosa2 * sina2, -1j * ejp * cosa],
+                    [em2jp * cosa2 * sina2, -cosa2 * sina2, 1j * emjp * cosa],
+                    [-0.5j * emjp * cosa, 0.5j * ejp * cosa, -sina] ])
+
+
+    FpFmZ = np.dot(RR, FpFmZ)
+
+    return FpFmZ, RR
+
+
+def relax_mat(T, T1, T2):
+    E2 = exp(-T/T2)
+    E1 = exp(-T/T1)
+
+    EE = np.diag([E2, E2, E1])      # Decay of states due to relaxation alone.
+
+    return EE
+
+def relax(FpFmZ, T, T1, T2):
     """ Propagate EPG states through a period of relaxation over
     an interval T.
 
@@ -66,14 +115,12 @@ def epg_relax(FpFmZ, T1, T2, T):
     OUTPUT:
         FpFmZ = updated F+, F- and Z states.
 
-    SEE ALSO:
-           epg_grad, epg_rf
    """
    
-    return epg_relax2(FpFmZ, T1, T2, T)[0]
+    return relax2(FpFmZ, T, T1, T2)[0]
 
 
-def epg_relax2(FpFmZ, T1, T2, T):
+def relax2(FpFmZ, T, T1, T2):
     """ Propagate EPG states through a period of relaxation over
     an interval T.
 
@@ -86,8 +133,6 @@ def epg_relax2(FpFmZ, T1, T2, T):
         FpFmZ = updated F+, F- and Z states.
         EE = decay matrix, 3x3 = diag([E2 E2 E1]);
 
-    SEE ALSO:
-           epg_grad, epg_rf
    """
 
     E2 = exp(-T/T2)
@@ -104,7 +149,7 @@ def epg_relax2(FpFmZ, T1, T2, T):
     return FpFmZ, EE
 
 
-def epg_grad(FpFmZ, noadd=False):
+def grad(FpFmZ, noadd=False):
     """Propagate EPG states through a "unit" gradient.
 
     INPUT:
@@ -116,8 +161,6 @@ def epg_grad(FpFmZ, noadd=False):
     OUTPUT:
         Updated FpFmZ state.
 
-    SEE ALSO:
-        epg_grad, epg_grelax
     """
 
     # Gradient does not affect the Z states.
@@ -133,7 +176,7 @@ def epg_grad(FpFmZ, noadd=False):
     return FpFmZ
 
 
-def epg_TE(FpFmZ, alpha, phi, T1, T2, TE, noadd=False):
+def FSE_TE(FpFmZ, alpha, phi, TE, T1, T2, noadd=False, recovery=True):
     """ Propagate EPG states through a full TE, i.e.
     relax -> grad -> rf -> grad -> relax
 
@@ -149,92 +192,107 @@ def epg_TE(FpFmZ, alpha, phi, T1, T2, TE, noadd=False):
     OUTPUT:
         FpFmZ = updated F+, F- and Z states.
 
-    SEE ALSO:
-           epg_grad, epg_rf, epg_relax
    """
 
-    FpFmZ = epg_relax(FpFmZ, T1, T2, TE/2.)
-    FpFmZ = epg_grad(FpFmZ, noadd)
-    FpFmZ = epg_rf(FpFmZ, alpha, phi)
-    FpFmZ = epg_grad(FpFmZ, noadd )
-    FpFmZ = epg_relax(FpFmZ, T1, T2, TE/2.)
+    EE = relax_mat(TE/2., T1, T2)
+
+    if recovery:
+        FpFmZ = relax(FpFmZ, TE/2., T1, T2)
+    else:
+        FpFmZ = np.dot(EE, FpFmZ)
+    FpFmZ = grad(FpFmZ, noadd)
+    FpFmZ = rf(FpFmZ, alpha, phi)
+    FpFmZ = grad(FpFmZ, noadd)
+    if recovery:
+        FpFmZ = relax(FpFmZ, TE/2., T1, T2)
+    else:
+        FpFmZ = np.dot(EE, FpFmZ)
 
     return FpFmZ
 
 
-def FSE_CPMG(angles_rad, TE, T1, T2):
-    """Simulate Fast Spin-Echo CPMG sequence with specific flip angle train.
-    Prior to the flip angle train, a 90 degree pulse is applied in the Y direction.
-    The flip angle train is then applied in the X direction.
+def FSE_TE_prime(FpFmZ, alpha, phi, TE, T1, T2, noadd=False, recovery=True):
+    """ Gradient of EPG propagatiom over a full TE, i.e.
+    relax -> grad -> rf_prime -> grad -> relax_hat,
+    where rf_prime is the derivative of the RF pulse matrix w.r.t. alpha,
+    and relax_hat  is the relaxation without longitudinal recovery
 
     INPUT:
-        angles_rad = array of flip angles in radians equal to echo train length
+        FpFmZ = 3xN vector of F+, F- and Z states.
+        alpha, phi = RF pulse flip angle and phase in radians
+        T1, T2 = Relaxation times (same as TE)
+        TE = Echo Time interval (same as T1, T2)
+        noadd = True to NOT add any higher-order states - assume
+                that they just go to zero.  Be careful - this
+                speeds up simulations, but may compromise accuracy!
+        recovery = True to include T1 recovery in the Z0 state.
+
+    OUTPUT:
+        FpFmZ = updated F+, F- and Z states.
+
+   """
+
+    FpFmZ, EE = relax2(FpFmZ, TE/2., T1, T2)
+    FpFmZ = grad(FpFmZ, noadd)
+    FpFmZ = rf_prime(FpFmZ, alpha, phi)
+    FpFmZ = grad(FpFmZ, noadd)
+    FpFmZ = np.dot(EE, FpFmZ)
+
+    return FpFmZ
+
+
+
+def FSE_signal(angles_rad, phase_rad, TE, T1, T2, M0=np.array([[0.],[0.],[1.]]), excitation_dict=None, inversion_dict=None):
+    """Same as FSE_signal2, but only returns Mxy"""
+    return FSE_signal2(angles_rad, phase_rad, TE, T1, T2, M0, excitation_dict=excitation_dict, inversion_dict=inversion_dict)[0]
+
+def FSE_signal2(angles_rad, phase_rad, TE, T1, T2, M0=np.array([[0.],[0.],[1.]]), excitation_dict=None, inversion_dict=None):
+    """Simulate Fast Spin-Echo sequence with specific flip angle/phase train.
+    The first flip angle is applied at time t=0. The second flip angle is applied at
+    time t=TE/2. Subsequent flip angles are spaced TE seconds apart.
+
+    INPUT:
+        angles_rad, phase_rad = array of flip angles and phase in radians
         TE = echo time/spacing
         T1 = T1 value in seconds
         T2 = T2 value in seconds
 
     OUTPUT:
-        Transverse magnetization at each echo time
+        Mxy = Transverse magnetization at each echo time
+        Mz = Longitudinal magnetization at each echo time
         
     """
 
-    S, S2 = FSE_CPMG2(angles_rad, TE, T1, T2)
+    T = len(angles_rad)
+    Mxy = np.zeros((T,1), dtype=complex)
+    Mz = np.zeros((T,1))
 
-    return S
+    P = np.copy(M0)
 
+    if inversion_dict != None:
+        alpha_TI, phi_TI, TI = inversion_dict['alpha_rad'], inversion_dict['phi_rad'], inversion_dict['TI']
+        P = rf(P, alpha_TI, phi_TI) # apply inversion pulse
+        P = relax(P, TI, T1, T2) # wait inversion time
 
-def FSE_CPMG2(angles_rad, TE, T1, T2):
-    """Same as FSE_CPMG, but returns Mxy and Mz"""
+    if excitation_dict != None:
+        alpha_e, phi_e = excitation_dict['alpha_rad'], excitation_dict['phi_rad']
+        P = rf(P, alpha_e, phi_e) # apply excitation pulse
+    else:
+        P = np.array([[1.], [1.], [0.]]) # 90 degree CPGM excitation
 
-    angles2_rad = np.insert(angles_rad, 0, pi/2)
-    phase2_rad = np.zeros(angles2_rad.shape)
-    phase2_rad[0] = pi/2
+    Mxy[0] = P[0,0]
+    Mz[0] = np.real(P[2,0])
 
-    return FSE_signal2(angles2_rad, phase2_rad, TE, T1, T2)
+    for i in xrange(T):
 
-
-def FSE_signal2(angles_rad, phase_rad, TE, T1, T2):
-    """Simulate Fast Spin Echo sequence with specific flip angles and phase.
-
-    INPUT:
-        angles_rad = array of RF flip angles in radians equal to (echo train length plus one)
-        phase_rad = array of RF phase in radians equal to (echo train length plus one)
-        TE = echo time/spacing
-        T1 = T1 value in seconds
-        T2 = T2 value in seconds
-
-    OUTPUT:
-        S1 -- Transverse magnetization at each echo time
-        S2 -- Longitudinal magnetization at each echo time
-
-    """
-
-    T = len(angles_rad) - 1 # don't include initial tip
-    S = np.zeros((T,1), dtype=complex)
-    S2 = np.zeros((T,1), dtype=complex)
-
-    P = np.array([[0],[0],[1]])    # initially in M0
-
-    P = epg_rf(P, angles_rad[0], phase_rad[0])  # initial tip
-
-    angles_rad = angles_rad[1:]
-    phase_rad = phase_rad[1:]
-
-    for i in range(T):
         alpha = angles_rad[i]
-        phase = phase_rad[i]
-        P = epg_relax(P, T1, T2, TE/2.)
-        P = epg_grad(P)
-        P = epg_rf(P, alpha, 0)
-        P = epg_grad(P)
-        P = epg_relax(P, T1, T2, TE/2.)
+        phi = phase_rad[i]
+        P = FSE_TE(P, alpha, phi, TE, T1, T2)
 
-        S[i] = P[0,0]
-        S2[i] = P[0,2]
+        Mxy[i] = P[0,0]
+        Mz[i] = np.real(P[2,0]) # real up to machine precision
 
-    return S, S2
-
-
+    return Mxy, Mz
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
